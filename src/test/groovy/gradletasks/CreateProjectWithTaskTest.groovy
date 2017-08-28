664ac23892a1f60d5202b94fa049101805bef3bf
Christian Wiedehöft <christian.wiedehoeft@signal-iduna.de>
2017-08-28 13:43:32 +0200

    Added example class for using gradle testkit

--- src/test/groovy/gradletasks/CreateProjectWithTaskTest.groovy
+++ src/test/groovy/gradletasks/CreateProjectWithTaskTest.groovy
@@ -1,15 +1,20 @@
 package gradletasks
 
-import org.gradle.internal.impldep.org.apache.tools.tar.TarOutputStream
-import org.gradle.testkit.runner.GradleRunner
-import org.junit.Rule
-import org.junit.rules.TemporaryFolder
 import static org.gradle.testkit.runner.TaskOutcome.*
 
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry
-import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+
 import spock.lang.Specification
 
+/**
+ * This class is currently only an example for using the gradle Testkit. Maybe we use it later
+ * for some integration tests.
+ *
+ */
+@Ignore
 class CreateProjectWithTaskTest extends Specification {
 
 	@Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
@@ -17,22 +22,48 @@ class CreateProjectWithTaskTest extends Specification {
 
 	def setup() {
 		buildFile = testProjectDir.newFile('build.gradle')
+		def thirdPartyDir = testProjectDir.newFolder("3rdParty")
 	}
+
+	def "create bsm64 directory for application"() {
+		def project = testProjectDir.newFolder("ProjectDirStub")
+		def buildDir = testProjectDir.newFolder(project.getName(), "gradleBuildDirStub")
+
+		def buildDirPath = buildDir.getPath()
+		given:
+		buildFile << """
+			apply plugin: 'java'
+			apply plugin: 'groovy'
+			task createBsm64Dir {
+				def bsm64Dir = new File($buildDirPath + '/bsm64')
+				bsm64Dir.mkDir()
+			}
+		"""
+
+		when:
+		def result = GradleRunner.create()
+				.withProjectDir(testProjectDir.root)
+				.withArguments('createBsm64Dir')
+				.build()
+
+		then:
+		buildDir.listFiles().length == 1
+		buildDir.listFiles()[0].getName().equals("bsm64")
+		result.task(":createBsm64Dir").outcome == SUCCESS
+	}
+
 
 	def "hello world task prints hello world"() {
 		given:
 		buildFile << """
-            task helloWorld {
-                doLast {
-                    println 'Hello world!'
-                }
-            }
+            task copy3rdParty {
+			}
         """
 
 		when:
 		def result = GradleRunner.create()
 				.withProjectDir(testProjectDir.root)
-				.withArguments('helloWorld')
+				.withArguments('copy3rdParty' , "--stacktrace")
 				.build()
 
 		then:
